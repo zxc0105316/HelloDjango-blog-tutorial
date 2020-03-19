74399d950110385b58871b2c9d26c17731938576
zxc0105316 <805403077@qq.com>
2020-03-19 15:21:48 +0800

    第三次上传

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
+# STATICFILES_DIRS = (
+#
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 11
