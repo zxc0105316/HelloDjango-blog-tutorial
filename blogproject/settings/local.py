001b52a676b088d9cac81857e13ee6716115faef
zxc0105316 <805403077@qq.com>
2020-03-19 20:43:38 +0800

    第四次上传

---
+++ blogproject/settings/local.py
@@ -0,0 +1,11 @@
+from .common import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# Development secret key
+SECRET_KEY = '_%8mkoucw$cu8%xeb1r%_@id346&al71ff*x_v)5tdacj#05rv'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
---
+++ blogproject/settings/production.py
@@ -0,0 +1,11 @@
+from .common import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# online SECRET_KEY
+SECRET_KEY = '2pe8eih8oah2_2z1=7f84bzme7^bwuto7y&f(#@rgd9ux9mp-3'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['www.zztzw.top']
